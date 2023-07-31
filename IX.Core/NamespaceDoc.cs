327fdbff4779f4eafff929e829fea05ec1aa6c4f
Adrian Moș <adimosh@outlook.com>
2023-07-31 11:56:41 +0300

    #10 - added some namespace documentation

---
+++ IX.Core/NamespaceDoc.cs
@@ -0,0 +1,13 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace IX.Core;
+
+/// <summary>
+/// This namespace contains general purpose classes, mostly the ground work for IX classes.
+/// </summary>
+[CompilerGenerated]
+[SuppressMessage("ReSharper", "UnusedType.Global", Justification = "Namespace documentation class.")]
+internal class NamespaceDoc
+{
+}
