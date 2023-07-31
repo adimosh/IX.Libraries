50246990537b0cbfb11eb338f713c1274fc600f5
Adrian Moș <adimosh@outlook.com>
2023-07-31 12:08:59 +0300

    Updated namespaces in serialization.

--- IX.Core/ComponentModel/Constants.cs
+++ IX.Core/ComponentModel/Constants.cs
@@ -2,5 +2,5 @@ namespace IX.Core.ComponentModel;
 
 internal static class Constants
 {
-    internal const string DataContractNamespace = "http://ns.ixiancorp.com/IX/IX.Core.ComponentModel";
+    internal const string DataContractNamespace = "http://adrianmos.eu/ns/IX/IX.Core.ComponentModel";
 }
