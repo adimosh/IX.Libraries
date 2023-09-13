b443145bb08a306f06643f34e40f596cb5e8f837
Adrian Moș <adimosh@outlook.com>
2023-09-13 10:37:16 +0300

    Moved IX.Core to IX.Library.

--- IX.Library/Resources.Designer.cs
+++ IX.Library/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace IX.Core {
+namespace IX.Library {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace IX.Core {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("IX.Core.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("IX.Library.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
